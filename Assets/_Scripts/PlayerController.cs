1b50b2247b0a9ec2288dc246addecad1455e7c80
muditsaxena1 <mudit.25102010@gmail.com>
2020-05-08 11:55:55 +0530

    Added 8 directional movement, 8 idle, 4 attack animations

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -26,7 +26,10 @@ public class PlayerController : MonoBehaviour
     {
         float xInpt = StepFunction(Input.GetAxisRaw("Horizontal"));
         float yInpt = StepFunction(Input.GetAxisRaw("Vertical"));
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
         MovePlayer(new Vector2(xInpt, yInpt));
     }
 
@@ -43,47 +46,68 @@ public class PlayerController : MonoBehaviour
         return 0f;
     }
 
-
-
+    void Attack()
+    {
+        anim.SetTrigger("Attack");
+    }
+    //LastPos Parameter is used to assign int for 8 directions
+    //3 4 5
+    //2 X 6
+    //1 0 7
+    //For example 5 for Up-Right direction
     void MovePlayer(Vector2 direction)
     {
         //Animation
-        switch (direction.x)
+        if(direction.x == 0 && direction.y == 0)
         {
-            case -1f:
-                anim.SetBool("Left",true);
-                anim.SetBool("Right", false);
-                break;
-            case 0f:
-                anim.SetBool("Left", false);
-                anim.SetBool("Right", false);
-                break;
-            case 1f:
-                anim.SetBool("Left", false);
-                anim.SetBool("Right", true);
-                break;
-            default:
-                Debug.LogWarning("direction.x should have values among -1,0,1. Found to be" + direction.x);
-                break;
+            anim.SetBool("KeyDown", false);
         }
-        switch (direction.y)
+        else if (direction.x == 0 && direction.y == -1)
         {
-            case -1f:
-                anim.SetBool("Down", true);
-                anim.SetBool("Up", false);
-                break;
-            case 0f:
-                anim.SetBool("Down", false);
-                anim.SetBool("Up", false);
-                break;
-            case 1f:
-                anim.SetBool("Down", false);
-                anim.SetBool("Up", true);
-                break;
-            default:
-                Debug.LogWarning("direction.y should have values among -1,0,1. Found to be" + direction.y);
-                break;
+            anim.SetInteger("LastPos", 0);
+            anim.SetBool("KeyDown", true);
         }
+        else if(direction.x == -1 && direction.y == -1)
+        {
+            anim.SetInteger("LastPos", 1);
+            anim.SetBool("KeyDown", true);
+        }
+        else if (direction.x == -1 && direction.y == 0)
+        {
+            anim.SetInteger("LastPos", 2);
+            anim.SetBool("KeyDown", true);
+        }
+        else if(direction.x == -1 && direction.y == 1)
+        {
+            anim.SetInteger("LastPos", 3);
+            anim.SetBool("KeyDown", true);
+        }
+        else if (direction.x == 0 && direction.y == 1)
+        {
+            anim.SetInteger("LastPos", 4);
+            anim.SetBool("KeyDown", true);
+        }
+        else if(direction.x == 1 && direction.y == 1)
+        {
+            anim.SetInteger("LastPos", 5);
+            anim.SetBool("KeyDown", true);
+        }
+        else if (direction.x == 1 && direction.y == 0)
+        {
+            anim.SetInteger("LastPos", 6);
+            anim.SetBool("KeyDown", true);
+
+        }
+        else if (direction.x == 1 && direction.y == -1)
+        {
+            anim.SetInteger("LastPos", 7);
+            anim.SetBool("KeyDown", true);
+        }
+        else
+        {
+            Debug.LogWarning("direction.x/y should have values among -1,0,1. Found to be " + direction.x + "/" + direction.y);
+        }
+            
         Vector2 currPos = new Vector2(transform.position.x, transform.position.y);
         if(Mathf.Abs(direction.x) == 1f && Mathf.Abs(direction.y) == 1f)
         {
