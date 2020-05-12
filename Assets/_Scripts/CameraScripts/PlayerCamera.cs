978378c470178c708bfe189179121c60074347fb
NishBrod <43043984+NishBrod@users.noreply.github.com>
2020-05-11 20:15:57 -0400

    changes
    player camera
    urp in kingdom_2
    tilemap colliders

---
+++ Assets/_Scripts/CameraScripts/PlayerCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothingEffect;
+    public GameObject minBound;
+    public GameObject maxBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (transform.position!=target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x,target.position.y,transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x,minBound.transform.position.x,maxBound.transform.position.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minBound.transform.position.y, maxBound.transform.position.y);
+            transform.position = Vector3.Lerp(transform.position,targetPosition,smoothingEffect);
+        }
+        
+    }
+}
