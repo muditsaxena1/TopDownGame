5c6e358eec67c102578b2faf6baa1b00b11b8427
muditsaxena1 <mudit.25102010@gmail.com>
2020-05-13 17:08:44 +0530

    Merge branch 'nishu_urp_branch' into mudit_branch

---
+++ Assets/Chats/Dialog_npc_1/chat_1.ink
@@ -0,0 +1,24 @@
+All hail King Xorkon! What do you want here, Peasant?
+*[Hello]I'm new around here.
+*[Don't be rude]you don't have to be so rude.
+-
+->Divert1
+
+==Divert1==
+
+Well you're wasting my time! Got things to take care of.
+*[Ask about King]Can you tell me about King?
+    His Majesty has been kind to his subjects.
+    **[Go back]->Divert1
+    
+*[Ask about People]What about the people?
+    They have been rejoicing in the rule of his Majesty, after all he liberated them from that Barbarian, Rowland.
+    **[Rowland?]Who's Rowland?
+        He ruled these lands of Asnaria before his Majesty Xorkon swept him away.
+        -- ->Divert1
+    **[Go back]->Divert1
+*[Leave]Sorry!I didn't mean to talk to you.
+->Endpart
+
+==Endpart==
+->END
---
+++ Assets/_Scripts/CameraScripts/PlayerCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothingEffect;
+    public GameObject minBound;
+    public GameObject maxBound;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minPosition = minBound.transform.position;
+        maxPosition = maxBound.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (transform.position!=target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x,target.position.y,transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x,minPosition.x,maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+            transform.position = Vector3.Lerp(transform.position,targetPosition,smoothingEffect);
+        }
+        
+    }
+}
